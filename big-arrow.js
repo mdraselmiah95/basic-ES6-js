d77b051a99d3cdf9a302a948b662644492f2460b
rasel <russelroy15@gmail.com>
2021-08-25 07:50:18 +0600

    more es6 practice

---
+++ big-arrow.js
@@ -0,0 +1,17 @@
+const add = (num1, num2) => num1 + num2;
+const sum = add(13, 10);
+// console.log(sum);
+
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+const result = multiply(2, 5, 8);
+console.log(result);
+
+const tenTimes = (num) => num * 10;
+const output = tenTimes(17);
+
+const fiveTimes = num => num * 5;
+const outputF = fiveTimes(10);
+
+const getName = () => 'Brandon Sam';
+const name = getName();
+console.log(name);
--- template-string.js
+++ template-string.js
@@ -18,13 +18,14 @@ const friends = ['Rasel', 'Rohim', 'Korim', 'Asif', 'Shakib'];
 const count = 5;
 const old = '<h3 class="friend-name">Friend-5</h3>'
 const old2 = '<h3 class="friend-name">Friend-' + count + '</h3>';
+const new0 = `<h3 class="friend-name">Friend-${meya}</h3>`
 const new1 = `<h3 class="friend-name">Friend-${count}</h3>'`;
 const new2 = `<h3 class="friend-name">Friend-${friends.length}</h3>`;
 console.log(new1);
-
+console.log(new0);
 const first = 'Rohim';
 const last = 'Khan';
 const fullOld = 'This person name is ' + first + ' ' + last;
 
 const fullNew = `This person name is: ${first} ${last}.Has money ${friends.length * 500} .He lives in Dhaka.`;
-console.log(fullNew)
+console.log(fullNew);
